067f896085aff68b87c95f7fb019bf59c7cf8dcc
Yuta <Yuta.Takahashi@cern.ch>
2014-04-30 10:36:42 +0200

    First upload emuTau analysis codes

---
+++ CMGTools/H2TauTau/prod/prod_emuTau/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#/eos/cms/store/cmst3/user/cmgtools/CMG/DoubleMu/StoreResults-Run2012C_PromptReco_v2_embedded_trans1_tau132_pttau1_17had2_17_v2-5ef1c0fd428eb740081f19333520fdc8/USER/V5_B/PAT_CMG_V5_14_0
+
+#ProductionTasks.py -w '*.root' -c -N 1 -q 2nd -t <PAT_CMG_X_X_X> --output_wildcard '*.root' --cfg <yuta's_skimming_cfg> `cat samples.txt` 
+#cmgtools%/DoubleMu/StoreResults-Run2012C_PromptReco_v2_embedded_trans1_tau132_pttau1_17had2_17_v2-5ef1c0fd428eb740081f19333520fdc8/USER/V5_B/PAT_CMG_V5_14_0
+
+ProductionTasks.py -w '*.root' -c -N 5 -q 1nd -t EMuTau_Yuta_Mar3 --batch_user htautau_group --output_wildcard '*.root' --cfg skim_emutau_cfg.py `cat samples_skim.txt`
---
+++ CMGTools/H2TauTau/python/proto/samples/run2012/triggers_mumuTau.py
@@ -0,0 +1,19 @@
+data_triggers = [
+    'HLT_IsoMu18_eta2p1_LooseIsoPFTau20_v*', # 2012A
+    'HLT_IsoMu17_eta2p1_LooseIsoPFTau20_v*'  # 2012B & C & D
+    ]
+
+
+mc_triggers = [
+    'HLT_IsoMu24_v15'
+#    'HLT_IsoMu24_eta2p1'
+    ]
+
+embed_triggers = [
+    'HLT_Mu17_Mu8_v16', 
+    'HLT_Mu17_Mu8_v17', 
+    'HLT_Mu17_Mu8_v18', 
+    'HLT_Mu17_Mu8_v19', 
+    'HLT_Mu17_Mu8_v21', 
+    'HLT_Mu17_Mu8_v22'
+    ]
