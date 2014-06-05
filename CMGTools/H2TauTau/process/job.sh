3a493e594de634c3306fb446aa1da8862926cdf0
Riccardo <riccardo.manzoni@cern.ch>
2014-06-05 12:53:59 +0200

    added yuta's cfg for ntuplization

---
+++ CMGTools/H2TauTau/process/job.sh
@@ -0,0 +1,4 @@
+#pybatch.py -o emt_20140212_v2 emutau_2013_cfg.py -b 'bsub -q 8nh -J emt_20140212_v2 < batchScript.sh'
+pybatch.py -o emt_20140423 emutau_2014_cfg.py -b 'bsub -q 8nh -J emt_20140423 < batchScript.sh'
+#pybatch.py -o TTV emutau_2014_cfg.py -b 'bsub -q 8nh -J TTV < batchScript.sh'
+#pybatch.py -o TTH test.py -b 'bsub -q 8nh -J TTH < batchScript.sh'
